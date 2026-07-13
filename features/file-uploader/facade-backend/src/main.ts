ccaf352309ff41d0da7a5f2557f04d58e8cebba4
Rafal Mirkowski <rafalmirkowski@gmail.com>
2026-07-13 21:39:52 +0200

    feature: slightly refactor

--- features/file-uploader/facade-backend/src/main.ts
+++ features/file-uploader/facade-backend/src/main.ts
@@ -6,13 +6,13 @@ import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const config = new ConfigService()
+  const configService = app.get(ConfigService)
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
       urls: ['amqp://localhost:5672'],
-      queue: config.getOrThrow('SERVICE_QUEUE_NAME'),
+      queue: configService.getOrThrow('SERVICE_QUEUE_NAME'),
       queueOptions: {
         durable: false,
       },
@@ -20,7 +20,7 @@ async function bootstrap() {
   })
 
   await app.startAllMicroservices()
-  await app.listen(8081)
+  await app.listen(configService.get('PORT') as string)
 }
 
 bootstrap()
