dde899dc306a2289efb89db963aacf1af687a458
Rafal Mirkowski <rafalmirkowski@gmail.com>
2026-09-02 20:02:23 +0200

    feature: create logging module

--- features/aws-lambda/lambda-backend/src/example-job/example-job.module.ts
+++ features/aws-lambda/lambda-backend/src/example-job/example-job.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common'
 import { ExampleJobService } from './example-job.service'
 import { ConfigModule } from '@nestjs/config'
 import { DrizzleModule } from '../db/drizzle.module'
-import { LoggerModule } from 'nestjs-pino'
+import { AppLoggerModule } from '../common/logger/logger.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        base: undefined,
-        messageKey: 'message',
-        timestamp: () => `,"time":"${new Date().toISOString()}"`,
-        formatters: { level: (label) => ({ level: label }) },
-      },
-    }),
     DrizzleModule,
+    AppLoggerModule,
   ],
   providers: [ExampleJobService],
 })
