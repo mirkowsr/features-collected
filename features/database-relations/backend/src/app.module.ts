455c896d09df704b989d6860550c494e219bdfc0
Rafal Mirkowski <rafalmirkowski@gmail.com>
2026-09-26 07:30:25 +0200

    add orders module

--- features/database-relations/backend/src/app.module.ts
+++ features/database-relations/backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { DrizzleModule } from './db/drizzle.module'
 import { CustomersModule } from './customers/customers.module'
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DrizzleModule,
     CustomersModule,
+    OrdersModule,
   ],
   controllers: [AppController],
 })
